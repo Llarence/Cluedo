36f752eaef7ed75380225fb2fca9b27981cf9e2a
Llarence <1010kai.t1010@gmail.com>
2023-05-11 10:32:41 -0700

    stop the original dialog being lost

--- src/main/kotlin/game/Main.kt
+++ src/main/kotlin/game/Main.kt
@@ -7,33 +7,37 @@ import game.client.HardAI
 import game.client.MediumAI
 import game.server.Server
 import java.net.InetAddress
+import javax.swing.JFrame
 import javax.swing.JOptionPane
 import javax.swing.UIManager
 import kotlin.system.exitProcess
 
+
+val onTopFrame = JFrame().apply { isAlwaysOnTop = true }
+
 fun initServer() {
-    val numPlayers = JOptionPane.showOptionDialog(null, "How many players?", "Please select", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, (3..6).toList().toTypedArray(), 0) + 3
+    val numPlayers = JOptionPane.showOptionDialog(onTopFrame, "How many players?", "Please select", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, (3..6).toList().toTypedArray(), 0) + 3
     if (numPlayers == 2) {
         exitProcess(0)
     }
 
-    val numEasyAIs = JOptionPane.showOptionDialog(null, "How many easy ais?", "Please select", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, (0..numPlayers).toList().toTypedArray(), 0)
+    val numEasyAIs = JOptionPane.showOptionDialog(onTopFrame, "How many easy ais?", "Please select", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, (0..numPlayers).toList().toTypedArray(), 0)
     if (numEasyAIs == -1) {
         exitProcess(0)
     }
 
-    val numMediumAIs = JOptionPane.showOptionDialog(null, "How many medium ais?", "Please select", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, (0..numPlayers - numEasyAIs).toList().toTypedArray(), 0)
+    val numMediumAIs = JOptionPane.showOptionDialog(onTopFrame, "How many medium ais?", "Please select", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, (0..numPlayers - numEasyAIs).toList().toTypedArray(), 0)
     if (numMediumAIs == -1) {
         exitProcess(0)
     }
 
-    val numHardAIs = JOptionPane.showOptionDialog(null, "How many hard ais?", "Please select", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, (0..numPlayers - numEasyAIs - numMediumAIs).toList().toTypedArray(), 0)
+    val numHardAIs = JOptionPane.showOptionDialog(onTopFrame, "How many hard ais?", "Please select", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, (0..numPlayers - numEasyAIs - numMediumAIs).toList().toTypedArray(), 0)
     if (numHardAIs == -1) {
         exitProcess(0)
     }
 
     val choice = if (numPlayers - numEasyAIs - numMediumAIs - numHardAIs > 0) {
-        JOptionPane.showOptionDialog(null, "What would you like to do?", "Please select", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, arrayOf("Observe", "Join"), 0)
+        JOptionPane.showOptionDialog(onTopFrame, "What would you like to do?", "Please select", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, arrayOf("Observe", "Join"), 0)
     } else {
         0
     }
@@ -85,7 +89,7 @@ fun initServer() {
             aiClients.add(aiClient)
         }
     } catch (_: Exception) {
-        JOptionPane.showMessageDialog(null, "Failed to connect")
+        JOptionPane.showMessageDialog(onTopFrame, "Failed to connect")
         exitProcess(0)
     }
 
@@ -99,14 +103,14 @@ fun initServer() {
 }
 
 fun initClient() {
-    val text = JOptionPane.showInputDialog(null, "Server address", "Please select", JOptionPane.NO_OPTION)
+    val text = JOptionPane.showInputDialog(onTopFrame, "Server address", "Please select", JOptionPane.NO_OPTION)
 
     if (text != null) {
         val client: DisplayClient
         try {
             client = DisplayClient(InetAddress.getByName(text), false)
         } catch (_: Exception) {
-            JOptionPane.showMessageDialog(null, "Failed to connect")
+            JOptionPane.showMessageDialog(onTopFrame, "Failed to connect")
             exitProcess(0)
         }
 
@@ -121,7 +125,7 @@ fun main() {
     } catch (_: Exception) {
     }
 
-    val choice = JOptionPane.showOptionDialog(null, "What would you like to do?", "Please select", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, arrayOf("Host", "Join"), 0)
+    val choice = JOptionPane.showOptionDialog(onTopFrame, "What would you like to do?", "Please select", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, arrayOf("Host", "Join"), 0)
     if (choice == 0) {
         initServer()
     }
@@ -129,4 +133,6 @@ fun main() {
     if (choice == 1) {
         initClient()
     }
+
+    exitProcess(0)
 }
